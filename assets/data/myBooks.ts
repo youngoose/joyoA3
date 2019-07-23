9381157d2e263766a74815c51c3b3b5a14afb1be
Eddy Jo <joyo@sheridancollege.ca>
2019-07-23 19:46:35 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,33 @@
+import { BookInfo } from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] =
+[
+  {
+    name: 'Pro Angular',
+    author: 'Adam Freeman',
+    genre: 'Web development',
+    year: 2017,
+    image: 'assets/images/Pro_ang.jpg'
+  },
+  {
+    name: 'UP&GOING JS',
+    author: 'Kyle Simpson',
+    genre: 'Web development',
+    year: 2015,
+    image: 'assets/images/JS_UP_GOING.jpg'
+  },
+  {
+    name: 'Mastering Angular Material',
+    author: 'Code Maze',
+    genre: 'Web development',
+    year: 2019,
+    image: 'assets/images/Ang_mat.jpg'
+  },
+  {
+    name: 'JS&JQUERY Interactive front-end web development',
+    author: 'Jon Duckett',
+    genre: 'Web development',
+    year: 2014,
+    image: 'assets/images/JS_JQ.jpg'
+  }
+]
